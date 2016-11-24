092ab60b9283c8ed08eb51a4baa2b17b86249769
Austin Wood <i@austinwood.me>
2016-11-23 16:58:34 -0800

    Fetch external title; store locally; render links to page; clear storage upon request

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,63 @@
-const button = document.querySelector('.alert')
+const parser = new DOMParser()
 
-button.addEventListener('click', () => {
-  alert(__dirname)
+const linksSection = document.querySelector('.links')
+const newLinkForm = document.querySelector('.new-link-form')
+const newLinkUrl = document.querySelector('.new-link-url')
+const newLinkSubmit = document.querySelector('.new-link-submit')
+const clearStorageButton = document.querySelector('.clear-storage')
+
+newLinkUrl.addEventListener('keyup', () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid
 })
+
+newLinkForm.addEventListener('submit', (e) => {
+  e.preventDefault
+
+  const url = newLinkUrl.value
+
+  fetch(url)
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle)
+    .then(title => storeLink(title, url))
+    .then(clearForm)
+    .then(renderLinks)
+})
+
+clearStorageButton.addEventListener('click', function clearStorage() {
+  localStorage.clear()
+  linksSection.innerHTML = ''
+})
+
+function clearForm() {
+  newLinkUrl.value = null
+}
+
+function parseResponse(text) {
+  return parser.parseFromString(text, 'text/html')
+}
+
+function findTitle(nodes) {
+  return nodes.querySelector('title').innerText
+}
+
+function storeLink(title, url) {
+  localStorage.setItem(url, JSON.stringify({ title, url }))
+}
+
+function getLinks() {
+  return Object.keys(localStorage)
+    .map(key => JSON.parse(localStorage.getItem(key)))
+}
+
+function convertToElement(link) {
+  return `<div class="link"><h3>${link.title}</h3>
+          <p><a href="${link.url}">${link.url}</a></p></div>`
+}
+
+function renderLinks() {
+  const linkElements = getLinks().map(convertToElement).join('')
+  linksSection.innerHTML = linkElements
+}
+
+renderLinks()
